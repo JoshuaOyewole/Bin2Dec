bbdf4cd90731291beac74eda936bc66922a7516d
Joshua Oyewole <joshpp2013@gmail.com>
2020-07-06 23:52:09 +0100

    Css Styles added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
-const submitbutton = document.getElementById('submit');
+let submitbutton = document.getElementById('submit');
 let outputField = document.getElementById('decimal');
 
  convertFunction = () =>{
     const getValue = document.getElementById('getBinary').value;
-    // Validate Functionality
+    const binary = parseInt(getValue, 2);
+    outputField.innerText = binary  ; 
 }
 
 submitbutton.addEventListener('click', convertFunction); 
